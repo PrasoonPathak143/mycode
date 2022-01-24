813de7cf0c730990b28724b7a69b6cbbebe5b14f
Prasoon <pp209738@gmail.com>
2022-01-25 00:13:55 +0530

    arr to ar

--- src/charAt.java
+++ src/charAt.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 public class charAt {
     public static void main(String[] args) {
         Scanner num = new Scanner(System.in);
-        int [] arr = new int[26];
+        int [] ar = new int[26];
         for(int i=0; i<26; i++){
-            arr[i] = num.nextInt();
+            ar[i] = num.nextInt();
         }
         String s = num.next();
         int l = 0;
         for(int i=97; i<=122; i++){
             for(int j=0; j<s.length(); j++){
               if(s.charAt(j)== i){
-                  if(arr[i-97]>l){
-                      l = arr[i-97];
+                  if(ar[i-97]>l){
+                      l = ar[i-97];
                   }
                   break;
               }
